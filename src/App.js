801982f6a8a24041519d7e03496c1ace2699b66e
kajalnayak <nayakkajal000@gmail.com>
2025-04-11 20:22:25 +0530

    For fixing Deploying Error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
+import { makeStyles } from '@material-ui/core';
+import CryptoContext from './CryptoContext'; 
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CryptoContext> 
+      <div className={classes.App}>
+        <BrowserRouter>
+          <Header />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/coins/:id" component={CoinPage} />
+        </BrowserRouter>
+      </div>
+    </CryptoContext>
   );
 }
 
